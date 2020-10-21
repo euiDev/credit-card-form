7152a9e783a77ecab3b1b999aacaaa72d75adbb3
Eugene Angelo <dev.eugene.paje@gmail.com>
2020-10-21 23:24:54 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CreditCardFormComponent } from './credit-card-form/credit-card-form.component';
+import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardFormComponent,
+    InputComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
